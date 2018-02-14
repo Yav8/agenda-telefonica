0263c79398bc5bf701184102df5d487441502294
Yav <yavieron@hotmail.es>
2018-02-14 23:55:29 +0100

    Creada clase AgendaTelefonica que permite introducir y leer números de teléfono de diferentes contactos

---
+++ AgendaTelefonica.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+/**
+ * Clase AgendaTelefonica que almacena números de 
+ * diferentes contactos.
+ */
+public class AgendaTelefonica {
+    private HashMap<String, String> agenda;
+    
+    /**
+     * Constructor de objetos de la clase AgendaTelefonica.
+     */
+    public AgendaTelefonica() {
+        agenda = new HashMap<>();
+    }
+    
+    /**
+     * Introduce el número de un contacto.
+     */
+    public void enterNumber(String name, String number) {
+        agenda.put(name, number);
+    }
+    
+    /**
+     * Devuelve el número de un contacto.
+     */
+    public String lookupNumber(String name) {
+        return agenda.get(name);
+    }
+}
